b2ffe8b21c918e336b943e078e702b5e1f3e4d44
Ankit Sharma <ankit.sharma25@gmail.com>
2020-10-27 15:58:54 +0530

    App Initialized

---
+++ src/components/card/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Card.css';
+
+export default () => (
+    <div className="card column">
+        <div className="card-content">
+        Test Card Data
+        </div>
+    </div>
+)
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../components/card/Card'
+export default function Home() {
+    return (
+     <div>
+        <div className="flex-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+    )
+}
